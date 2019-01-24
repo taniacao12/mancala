6a187a9b8d50a30bf58b59802d46f390190de466
Tania Cao <tcaosu@stuy.edu>
2019-01-24 17:15:37 -0500

    update

--- server.c
+++ server.c
@@ -44,10 +44,9 @@ int main() {
 
 void subserver(int player1, int player2) {
   char buffer[BUFFER_SIZE];
+  strcpy(buffer, "44444404444440");
   
   while (1) {
-    strcpy(buffer, "44444404444440");
-	
     // permits player1 to play
     write(player1, buffer, sizeof(buffer));
 	
